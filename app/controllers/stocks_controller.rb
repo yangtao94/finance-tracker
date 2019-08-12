2c57cc1f1499ccb1f713410f18163071294dbaf4
Yangtao Hu <yangt.1994@gmail.com>
2019-08-12 16:37:19 -0700

    Added error handling and refactored code for symbol lookup

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,19 +1,13 @@
 class StocksController < ApplicationController
   def search
-    if params[:stock].present?
-        @stock = Stock.new_from_lookup(params[:stock])
-      if @stock
-        respond_to do |format| 
-          format.js {render partial:'users/result'}
-        end
-        
-      else
-        flash[:danger] = "Please enter a valid symbol"
-        redirect_to my_portfolio_path
-      end
+    if params[:stock].blank?
+        flash.now[:danger] = "Please enter a symbol"
     else
-        flash[:danger] = "Please enter a symbol"
-        redirect_to my_portfolio_path
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = "Please enter a valid symbol" unless @stock  
+    end
+    respond_to do |format| 
+        format.js {render partial:'users/result'}
     end
   end
 
