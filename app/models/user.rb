8ec9a920218cbb9f238308cb0b90c6ae6a295201
Yangtao Hu <yangt.1994@gmail.com>
2019-08-15 14:17:53 -0700

    Added in ability to add and remove friends, and view their profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,8 +31,35 @@ class User < ApplicationRecord
   end
 
 
-  def my_friends
+  def self.search(param)
+    param.strip!
+    param.downcase!
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    return nil unless to_send_back
+    to_send_back
+  end
 
+  def self.first_name_matches(param)
+    matches("first_name",param)
+  end
   
+  def self.last_name_matches(param)
+    matches("last_name",param)
+  end
+  
+  def self.email_matches(param)
+    matches("email",param)
+  end
+
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
+  end
+
+  def except_current_user(users)
+    users.reject{|user| user.id == self.id}
+  end
+
+  def not_friends_with?(friend_id)
+    friendships.where(friend_id: friend_id).count <1
   end
 end
